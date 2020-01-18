79a6f79a39d534e2f50006b1791ef61b1a1832b6
UnLink <starzs3cret@gmail.com>
2020-01-18 12:22:42 +0700

    finish tutor njay easy

---
+++ myconsumerappnote/src/main/java/id/unlink/myconsumerappnote/helper/MappingHelper.java
@@ -0,0 +1,35 @@
+package id.unlink.myconsumerappnote.helper;
+
+import android.database.Cursor;
+
+import java.util.ArrayList;
+
+import id.unlink.myconsumerappnote.db.DatabaseContract;
+import id.unlink.myconsumerappnote.entity.Note;
+
+public class MappingHelper {
+
+    public static ArrayList<Note> mapCursorToArrayList(Cursor notesCursor) {
+        ArrayList<Note> notesList = new ArrayList<>();
+
+        while (notesCursor.moveToNext()) {
+            int id = notesCursor.getInt(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID));
+            String title = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE));
+            String description = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION));
+            String date = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE));
+            notesList.add(new Note(id, title, description, date));
+        }
+
+        return notesList;
+    }
+
+    public static Note mapCursorToObject(Cursor notesCursor) {
+        notesCursor.moveToFirst();
+        int id = notesCursor.getInt(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID));
+        String title = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE));
+        String description = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION));
+        String date = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE));
+
+        return new Note(id, title, description, date);
+    }
+}
